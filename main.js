2aa24ae3a645344a002cddf50177e06387a2e936
Ole Kröger <o.kroeger@wikunia.de>
2014-05-29 08:20:23 +0200

    bugfixes for userdefined tags

--- main.js
+++ main.js
@@ -36,7 +36,8 @@ define(function(require, exports, module) {
         var func = get_func_name(currentDoc,sel.start);
 		var func_name = func[0];
 		var func_class = func[1];
-     	console.log(func);
+     
+		
         // if a function was selected
         if (func_name) {
             // Initialize the Ajax request
@@ -67,10 +68,10 @@ define(function(require, exports, module) {
 				}
                 // try userdefined tags
                 if (!tags) {
-                  var func = new Object();
-                  func.name = func_name;
-                  tags = get_userdefined_tags(currentDoc,func);
-                  var url = null;
+					var func = new Object();
+					func.name = func_name;
+					tags = get_userdefined_tags(currentDoc,func);
+					var url = null;
                 } else {
                     var url = func_name;
                 }
@@ -140,7 +141,6 @@ define(function(require, exports, module) {
 					// func_class (without $)
 					if (class_pos != -1) {
 						var varClass = line.substr(pos.ch-class_pos,class_pos-b-2);
-						console.log('varClass: ' + varClass);
 						func_class = getClass(content,varClass);
 					}
 				} else {
@@ -177,13 +177,12 @@ define(function(require, exports, module) {
             var match_len = match[0].length;
         } else {
             // if the declaration is not available in this content
-            return 'unknown';   
+            return null;   
         }
 		
 		// get Class Value
 		var value = content.substr(pos+match_len,content.substr(pos+match_len).search(/[(;,]/));
         value = value.trim();
-		console.log('ClassName: '+value);
 		return value;
 	}
     
@@ -233,21 +232,30 @@ define(function(require, exports, module) {
                     }
                     tags.y = ''; // syntax is empty for this
                     
+					if (lines[i].substr(0,6) === '@param' || lines[i].substr(0,7) === '@return') {
+						canbe_des = false; // description tag closed
+					}
+					
                     // get params
                     if (lines[i].substr(0,6) === '@param') {
-                        canbe_des = false; // description tag closed
                         var param_parts = lines[i].split(/(?:\s+)/);
-                       
+                        var param_parts_length = param_parts.length;
                         // 0 = @param, 1 = title, 2-... = description
 						// 1 can be the type (not starting with a $) => 2 is the title (phpDoc)
                         // 2 can be the type (inside {}) (JavaDoc)
 						if (param_parts[1].substr(0,1) !== '$') {
 							// type is part of the title
-                            var param_title = param_parts[2] + ' {' + param_parts[1] + '}'; 
-                            var description = param_parts[3];
-                            var j_start = 4;	
+							if (param_parts_length > 2 && param_parts[2].substr(0,1) == '$') {
+                            	var param_title = param_parts[2] + ' {' + param_parts[1] + '}';
+								var description = param_parts[3];
+								var j_start = 4;
+							} else {
+								var param_title = "$"+param_parts[1];
+								var description = param_parts[2];
+								var j_start = 3;
+							}                            	
 						} else { // maybe JavaDoc
-							if (param_parts[2].substr(0,1) == '{' && param_parts[2].substr(-1) == '}') {
+							if (param_parts_length > 2 && param_parts[2].substr(0,1) == '{' && param_parts[2].substr(-1) == '}') {
 								// type is part of the title
 								var param_title = param_parts[1] + ' ' + param_parts[2]; 
 								var description = param_parts[3];
@@ -258,7 +266,7 @@ define(function(require, exports, module) {
 								var j_start = 3;
 							}
 						}
-                        for (var j = j_start; j < param_parts.length; j++) {
+                        for (var j = j_start; j < param_parts_length; j++) {
                             description += ' ' + param_parts[j];
                         }
                         params.push({'t':param_title,'d':description});
